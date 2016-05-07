13aac1415cb7a78f1cf1d15aa64acd66f7e726fe
stefan2122 <stefan@therawjuicery.com>
2016-05-07 14:03:41 -0700

    lesson 4 first try pushing

---
+++ gitSwitcher.js
@@ -0,0 +1,25 @@
+var weekday = 'monday'
+
+switch(weekday) {
+	case 'monday':
+		console.log('case of the mondays');
+		break;
+	case 'tuesday':
+		console.log('taco tuesday!');
+		break;
+	case 'wednesday':
+		console.log('Middle of the week wednesday');
+		break;
+	case 'thursday':
+		console.log('Thursdee Fundee')
+		break;
+	case 'friday':
+		console.log('TGIF');
+		break;
+	case 'saturday':
+		console.log('lets get turnt');
+		break;
+	case 'sunday':
+		console.log('sleep day');
+		break;
+}
